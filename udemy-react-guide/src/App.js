87f141e55c340cd255341ee607d2d81eaa162517
Yegor Yegorenkov <yyegorenkov@connecture.com>
2019-01-25 11:48:32 +0200

    Added preferred conditional display of list

--- udemy-react-guide/src/App.js
+++ udemy-react-guide/src/App.js
@@ -40,29 +40,31 @@ class App extends Component {
     };
 
     render() {
+        let persons = null;
+        if (this.state.showPersons) {
+            persons = (
+                <div>
+                    <Person
+                        name={this.state.persons[0].name}
+                        age={this.state.persons[0].age}/>
+                    <Person
+                        name={this.state.persons[1].name}
+                        age={this.state.persons[1].age}
+                        click={this.switchNameHandler.bind(this, 'new Name2')}>Test my nest
+                    </Person>
+                    <Person
+                        name={this.state.persons[2].name}
+                        age={this.state.persons[2].age}
+                        changed={this.nameChangeHandler}/>
+                </div>
+            );
+        }
         return (
             <div className="App">
                 <h1>Hello World!</h1>
                 <p>This actually works</p>
                 <button onClick={this.togglePersons}>Toggle list</button>
-                {
-                    this.state.showPersons ?
-                        <div>
-                            <Person
-                                name={this.state.persons[0].name}
-                                age={this.state.persons[0].age}/>
-                            <Person
-                                name={this.state.persons[1].name}
-                                age={this.state.persons[1].age}
-                                click={this.switchNameHandler.bind(this, 'new Name2')}>Test my nest
-                            </Person>
-                            <Person
-                                name={this.state.persons[2].name}
-                                age={this.state.persons[2].age}
-                                changed={this.nameChangeHandler}/>
-                        </div>
-                        : null
-                }
+                {persons}
             </div>
         );
     }
