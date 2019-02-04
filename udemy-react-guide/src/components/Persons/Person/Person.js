7509eb0dc423744de6350ce5069713bfea875c9f
Yegor Yegorenkov <yyegorenkov@connecture.com>
2019-02-04 11:46:47 +0200

    Updated

--- udemy-react-guide/src/components/Persons/Person/Person.js
+++ udemy-react-guide/src/components/Persons/Person/Person.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, {Component} from 'react'
 import styles from './Person.module.css'
 
-const person = (props) => {
-    return (
-        <div className={styles.Person}>
-            <p>I'm a {props.name} and i`m {props.age} years old</p>
-            <input type="text" onChange={props.changed} value={props.name}/>
-            <p><button onClick={props.deletePerson}>Delete</button></p>
-
-        </div>
-    )
+class Person extends Component {
+    render() {
+        return (
+            <div className={styles.Person}>
+                <p>I'm a {this.props.name} and i`m {this.props.age} years old</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name}/>
+                <p>
+                    <button onClick={this.props.deletePerson}>Delete</button>
+                </p>
+            </div>
+        )
+    }
 };
 
-export default person;
+export default Person;
